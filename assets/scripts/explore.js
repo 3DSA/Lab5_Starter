595df21b77f0a28fb43fd061ed7f4c18db93d296
3DSA <dsusheelkar@ucsd.edu>
2024-05-05 19:04:11 -0700

    testing branch should be wrong

--- assets/scripts/explore.js
+++ assets/scripts/explore.js
@@ -3,5 +3,32 @@
 window.addEventListener('DOMContentLoaded', init);
 
 function init() {
-  // TODO
+  const select = document.querySelector('#voice-select');
+  const button = document.querySelector('button');
+  const textarea = document.querySelector('textarea');
+  const image = document.querySelector('img');
+
+  let voices = [];
+  function populateVoices() {
+    voices = speechSynthesis.getVoices();
+    voices.forEach(voice => {
+      const option = document.createElement('option');
+      option.value = voice.name;
+      option.textContent = `${voice.name} (${voice.lang})`;
+      select.appendChild(option);
+    });
+  }
+
+  speechSynthesis.addEventListener('voiceschanged', populateVoices);
+
+  button.addEventListener('click', () => {
+    const utterance = new SpeechSynthesisUtterance(textarea.value);
+    const selectedVoiceName = select.selectedOptions[0].value;
+    utterance.voice = voices.find(voice => voice.name === selectedVoiceName);
+    image.src = 'assets/images/smiling-open.png';
+    speechSynthesis.speak(utterance);
+    utterance.onend = () => {
+      image.src = 'assets/images/smiling.png';
+    };
+  });
 }
--- assets/scripts/expose.js
+++ assets/scripts/expose.js
@@ -3,5 +3,39 @@
 window.addEventListener('DOMContentLoaded', init);
 
 function init() {
-  // TODO
+  const jsConfetti = new JSConfetti();
+  const img = document.querySelector('img');
+  const audio = document.querySelector('audio');
+  const select = document.getElementById('horn-select');
+  const volume = document.getElementById('volume');
+  const button = document.querySelector('button');
+  const volumeIcon = document.querySelector('div > img');
+
+  select.addEventListener('change', function() {
+    const selectedHorn = select.value;
+    img.src = `./assets/images/${selectedHorn}.svg`;
+    audio.src = `./assets/audio/${selectedHorn}.mp3`;
+  });
+
+  volume.addEventListener('input', function() {
+    const vol = volume.value;
+    audio.volume = vol / 100;
+
+    if (vol == 0) {
+      volumeIcon.src = './assets/icons/volume-level-0.svg';
+    } else if (vol < 33) {
+      volumeIcon.src = './assets/icons/volume-level-1.svg';
+    } else if (vol < 67) {
+      volumeIcon.src = './assets/icons/volume-level-2.svg';
+    } else {
+      volumeIcon.src = './assets/icons/volume-level-3.svg';
+    }
+  });
+
+  button.addEventListener('click', function() {
+    audio.play();
+    if (select.value == 'party-horn') {
+      jsConfetti.addConfetti();
+    }
+  });
 }
